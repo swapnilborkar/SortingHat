5b8debcd4871bb6d4445459afba67467f065d8b3
Swapnil Borkar <swapnilborkar@live.com>
2016-05-31 11:23:16 +0530

    Added cards to display images and links

--- index.js
+++ index.js
@@ -81,26 +81,60 @@ function sendGenericMessage(sender) {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "title": "Gryffindor!",
+                    "subtitle": "Oh, another Weasley!",
+                    "image_url": "http://66.media.tumblr.com/675069de1700426362c8f4f934d96161/tumblr_inline_n1sirqxtib1r31u6a.gif",
                     "buttons": [{
                         "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
+                        "url": "http://pottermore.wikia.com/wiki/Gryffindor",
+                        "title": "Aparecium"
                     }, {
                         "type": "postback",
                         "title": "Postback",
                         "payload": "Payload for first element in a generic bubble",
                     }],
                 }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "title": "Slytherin!",
+                    "subtitle": "Slytherin will help you on the way to greatness",
+                    "image_url": "https://images.pottermore.com/bxd3o8b291gf/5fWByiKsHmU6ccYQ6oqAWk/941bc4445349e194bfa61eaa578f39e7/Slytherin-PM-Crest.jpg?w=1200",
                     "buttons": [{
+                        "type": "web_url",
+                        "url": "http://pottermore.wikia.com/wiki/Slytherin",
+                        "title": "Aparecium"
+                    }, {
                         "type": "postback",
                         "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                },
+
+                {
+                    "title": "Ravenclaw!",
+                    "subtitle": "Wit beyond measure is man's greatest treasure",
+                    "image_url": "https://lh3.googleusercontent.com/-SEooZ27bQFY/VRhYQq7KqnI/AAAAAAAABI4/dtdiD7J3tqA/w960-h960/Ravenclaw%2B-%2BKerby%2BRosanes-done.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "http://pottermore.wikia.com/wiki/Ravenclaw",
+                        "title": "Aparecium"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                },
+
+                {
+                    "title": "Hufflepuff",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://img05.deviantart.net/2ebe/i/2014/155/8/4/hufflepuff_crest_by_chromomaniac-d7l0e4k.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "http://pottermore.wikia.com/wiki/Hufflepuff",
+                        "title": "Aparecium"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
                     }],
                 }]
             }
